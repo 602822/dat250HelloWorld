eff50abfbe65d3e65792b18332ee60c18a1dad63
602822 <89077126+602822@users.noreply.github.com>
2024-08-28 10:46:52 +0200

    hello world

---
+++ src/test/java/com/example/dat250HelloWorld/Dat250HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat250HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
