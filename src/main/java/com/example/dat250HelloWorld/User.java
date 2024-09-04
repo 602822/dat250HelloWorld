598b177292f566bf2dfa71970a80313406771005
602822 <89077126+602822@users.noreply.github.com>
2024-09-04 21:38:02 +0200

    added methods to the vote controller

--- src/main/java/com/example/dat250HelloWorld/User.java
+++ src/main/java/com/example/dat250HelloWorld/User.java
@@ -6,22 +6,16 @@ import java.util.List;
 public class User {
     String username;
     String email;
+    List<Vote> votes;
 
-
-    public User(String username, String email, List<Poll> userPolls) {
+    public User(String username, String email, List<Vote> votes) {
         this.username = username;
         this.email = email;
-
-    }
-
-    public User(String username, String email) {
-        this.username = username;
-        this.email = email;
-
+        this.votes = votes;
     }
 
     public User() {
-
+    votes = new ArrayList<>();
     }
 
     public String getEmail() {
@@ -40,12 +34,20 @@ public class User {
         this.username = username;
     }
 
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
 
     @Override
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
-                ", email='" + email +
+                ", email='" + email + '\'' +
+                ", votes=" + votes +
                 '}';
     }
 }
--- src/main/java/com/example/dat250HelloWorld/VoteController.java
+++ src/main/java/com/example/dat250HelloWorld/VoteController.java
@@ -1,16 +1,55 @@
 package com.example.dat250HelloWorld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class VoteController {
 
+    private final PollManager pollManager;
 
-    @GetMapping("/users/{username}/vote")
-    public String vote(@PathVariable String username) {
+    public VoteController(PollManager pollManager) {
+        this.pollManager = pollManager;
+    }
 
+    @GetMapping("/users/{username}/votes")
+    public ResponseEntity<List<Vote>> getUserVotes(@PathVariable String username) {
+        HashMap<String, User> users = pollManager.users;
+        if (users.containsKey(username)) {
+            User user = users.get(username);
+            List<Vote> votes = user.getVotes();
+            return new ResponseEntity<>(votes, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @PostMapping("/users/{username}/votes")
+    public ResponseEntity<Vote> createUserVote(@PathVariable String username, @RequestBody Vote vote) {
+        HashMap<String, User> users = pollManager.users;
+        if (users.containsKey(username)) {
+            User user = users.get(username);
+            List<Vote> votes = user.getVotes();
+            votes.add(vote);
+            return new ResponseEntity<>(vote, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/users/{username}/votes/{id}")
+    public ResponseEntity<Vote> updateUserVote(@PathVariable String username, @RequestBody Vote vote, @PathVariable int id) {
+        HashMap<String, User> users = pollManager.users;
+        if (users.containsKey(username)) {
+            User user = users.get(username);
+            List<Vote> votes = user.getVotes();
+            votes.set(id, vote);
+            return new ResponseEntity<>(vote, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
