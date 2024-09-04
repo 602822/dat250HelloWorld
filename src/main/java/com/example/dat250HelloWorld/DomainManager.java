285ca0c2c8fbcf2bfc8e5b8c30f2639f20e981ae
602822 <89077126+602822@users.noreply.github.com>
2024-09-04 10:45:05 +0200

    added the domain and poll manager

---
+++ src/main/java/com/example/dat250HelloWorld/DomainManager.java
@@ -0,0 +1,8 @@
+package com.example.dat250HelloWorld;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DomainManager {
+}
--- src/main/java/com/example/dat250HelloWorld/PollManager.java
+++ src/main/java/com/example/dat250HelloWorld/PollManager.java
@@ -1,7 +1,23 @@
 package com.example.dat250HelloWorld;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class PollManager {
-    HashMap<User, Poll> userPolls;
+    HashMap<User, List<Poll>> userPolls;
+
+    public PollManager(HashMap<User, List<Poll>> userPolls) {
+        this.userPolls = userPolls;
+    }
+
+    public PollManager() {
+    }
+
+    public HashMap<User, List<Poll>> getUserPolls() {
+        return userPolls;
+    }
+
+    public void setUserPolls(HashMap<User, List<Poll>> userPolls) {
+        this.userPolls = userPolls;
+    }
 }
